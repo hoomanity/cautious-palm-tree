8c87c90496cbd67598c8a0653e4370f1261db149
hooman <87627772+hoomanity@users.noreply.github.com>
2024-02-28 22:42:10 +1100

    making the navbar links and style of navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from "../img/logo.jpg"
 
 function Navbar() {
   return (
     <div className='navbar'>
       <div className="container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art"><h6>ART</h6></Link>
+          <Link className="link" to="/?cat=print"><h6>PRINT</h6></Link>
+          <Link className="link" to="/?cat=ball"><h6>BALL</h6></Link>
+          <Link className="link" to="/?cat=mango"><h6>MANGO</h6></Link>
+          <Link className="link" to="/?cat=cat"><h6>CAT</h6></Link>
+          <Link className="link" to="/?cat=food"><h6>FOOD</h6></Link>
+          <span>Hooman</span>
+          <span>Logout</span>
+          <span className='write'><Link className='link' to="/write">Write</Link></span>
+        </div>
       </div>
-      </div>
+    </div>
   )
 }
 
