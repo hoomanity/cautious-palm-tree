779ba524f04dd67329ca0ab8681b51f43a081a6d
hooman <87627772+hoomanity@users.noreply.github.com>
2024-03-01 00:22:24 +1100

    single post

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+   <div className="single">
+    <div className="content">
+      <img src="https://images.pexels.com/photos/20340989/pexels-photo-20340989/free-photo-of-cat-next-to-fern-leaves.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+    </div>
+    <div className="menu">menu</div>
+   </div>
   )
 }
 
