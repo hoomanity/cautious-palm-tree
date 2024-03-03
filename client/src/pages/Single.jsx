8cb3690d403670c780058d4b573cc2cdc179c5e7
hooman <87627772+hoomanity@users.noreply.github.com>
2024-03-03 18:59:18 +1100

    adding single post page with edit and delete buttons (the edit button will direct to write page and delete button just exists for now)

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import {Link} from 'react-router-dom'
+
 
 const Single = () => {
   return (
    <div className="single">
     <div className="content">
       <img src="https://images.pexels.com/photos/20340989/pexels-photo-20340989/free-photo-of-cat-next-to-fern-leaves.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+    <div className="user">
+      <img src="https://images.pexels.com/photos/19477996/pexels-photo-19477996/free-photo-of-close-up-of-a-white-cat.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+      <div className="info">
+        <span>hooman</span>
+        <p>posted 2 days ago</p>
+      </div>
+      <div className="edit">
+        <Link to={`/write?edit=2`}>
+        <img src={Edit} alt="" />
+        </Link>
+        <img src={Delete} alt="" />
+      </div>
+    </div>
     </div>
     <div className="menu">menu</div>
    </div>
